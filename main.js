7e5eaa7dfd63ebb65483608740871fe2d46ffea1
soutkari <david.siven@gmail.com>
2017-10-09 10:04:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ $(function() {
 	  var tpl = $('#newtmp').html(),html = Mustache.to_html(tpl, users);
 	  $("#profiles").append(html);
 	  $(window).unbind('beforeunload');
-	  document.getElementById("link").href = window.redirect+'&p='+window.participant+'&c='+window.condition+'&u='+encodeURI(window.username)+'&av='+window.avatarexport+'&d='+encodeURI(window.description)+'&w='+window.words;
+	  document.getElementById("link").href = window.redirect+'&p='+window.participant+'&c='+window.condition+'&u='+encodeURI(window.username)+'&av='+window.avatarexport+'&r='+window.religion+'&d='+encodeURI(window.description)+'&w='+window.words;
   }
 
   // **Slide:** **Username**       
